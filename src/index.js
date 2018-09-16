bb9e3d72ac010124848710821bdb31efc3f55174
TGFreY <t.g.frey19@gmail.com>
2018-09-16 21:43:54 +0300

    zeros counter trick

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    let zeros = 0;
+    while ((Math.floor(number / 5)) > 0) {
+        number = Math.floor(number / 5);
+        zeros += number;
+    }
+    return zeros;
 }
